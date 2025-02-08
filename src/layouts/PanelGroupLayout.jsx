bf669365e190c76096c3e16e07edde2b8074555e
githubhenrykoo <lckoo1230@gmail.com>
2025-02-08 13:11:02 +0800

    index2

--- src/layouts/PanelGroupLayout.jsx
+++ src/layouts/PanelGroupLayout.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { Provider, useSelector } from 'react-redux';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import { store } from '../store';
 
 const PanelContent = () => {
@@ -17,11 +18,23 @@ const PanelContent = () => {
 
   return (
     <div className="h-screen overflow-hidden">
-      <div id="panel-layout" className="h-full">
-        <div data-panel="left">{renderPanel(panels.left)}</div>
-        <div data-panel="middle">{renderPanel(panels.middle)}</div>
-        <div data-panel="right">{renderPanel(panels.right)}</div>
-      </div>
+      <PanelGroup direction="horizontal" className="h-full">
+        <Panel defaultSize={panels.left.size} minSize={panels.left.minSize}>
+          {renderPanel(panels.left)}
+        </Panel>
+
+        <PanelResizeHandle className="w-1 bg-slate-200 hover:bg-slate-400 transition-colors" />
+
+        <Panel defaultSize={panels.middle.size} minSize={panels.middle.minSize}>
+          {renderPanel(panels.middle)}
+        </Panel>
+
+        <PanelResizeHandle className="w-1 bg-slate-200 hover:bg-slate-400 transition-colors" />
+
+        <Panel defaultSize={panels.right.size} minSize={panels.right.minSize}>
+          {renderPanel(panels.right)}
+        </Panel>
+      </PanelGroup>
     </div>
   );
 };

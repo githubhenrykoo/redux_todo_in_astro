17cb00ad5130c6f74e010bdf2c66b5c6bd0d7113
ronysinaga <daffa.padantya12@gmail.com>
2025-03-05 13:00:25 +0800

    name mapping

---
+++ Docs/config/name_mapping.py
@@ -0,0 +1,10 @@
+NAME_MAPPING = {
+    'githubhenrykoo': 'Henry Koo',
+    'daffapadantya12': 'Daffa Padantya',
+    'ronysinaga': 'Rony Sinaga',
+    'benkoo': 'Ben Koo',
+    'angelitadp' : 'Angelita',
+
+    # Add more mappings as needed:
+    # 'github_username': 'Real Name',
+}

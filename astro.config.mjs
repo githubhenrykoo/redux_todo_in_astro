d428e1b6713bc624c2488168a38672bb96eee312
Henry Koo <lckoo1230@gmail.com>
2025-04-25 11:19:01 +0800

    Update allowedHosts and reset playwright test state to idle

--- astro.config.mjs
+++ astro.config.mjs
@@ -153,7 +153,7 @@ export default defineConfig({
       strictPort: true,
       proxy: {},
       cors: true,
-      allowedHosts: ['localhost', '127.0.0.1', 'todo.pkc.pub', '10.243.143.134', '172.22.1.146', 'todo.toyhouse.cc', 'todo.web4x.org']
+      allowedHosts: ['localhost', '127.0.0.1', 'todo.pkc.pub', '10.243.143.134', '172.22.1.146', 'todo.toyhouse.cc', 'todo.web4x.org', 'alessandro.pkc.pub']
     },
     build: {
       rollupOptions: {

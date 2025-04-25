73304546f95713d29afc30ff411030933a5f66d4
Henry Koo <lckoo1230@gmail.com>
2025-04-25 11:21:50 +0800

    Update Vite server config to allow all hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -153,7 +153,8 @@ export default defineConfig({
       strictPort: true,
       proxy: {},
       cors: true,
-      allowedHosts: ['localhost', '127.0.0.1', 'todo.pkc.pub', '10.243.143.134', '172.22.1.146', 'todo.toyhouse.cc', 'todo.web4x.org', 'alessandro.pkc.pub']
+      host: true, 
+      allowedHosts: true 
     },
     build: {
       rollupOptions: {

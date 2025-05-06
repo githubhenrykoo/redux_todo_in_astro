d45259c11120a07d0eda3f95437efd41b5c0205a
Alessandro Rumampuk <44091930+alessandrorumampuk@users.noreply.github.com>
2025-05-06 10:46:43 +0800

    Update Playwright_Python_REPL.js

--- src/pages/api/Playwright_Python_REPL.js
+++ src/pages/api/Playwright_Python_REPL.js
@@ -19,48 +19,48 @@ import { chromium } from 'playwright';
   await page.goto('http://localhost:4321');
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step1.png' });
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
 
   await page.click('a.enter-button');
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step2.png' });
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
 
   const clmElement = await page.locator('pre', { hasText: '"type":"clm_document"' }).first();
   await clmElement.scrollIntoViewIfNeeded();
   await clmElement.click();
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step3.png' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   const runningText1 = await page.locator('h3', { hasText: 'Python Interactive Console' }).first();
   await runningText1.scrollIntoViewIfNeeded();
   await runningText1.focus();
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step4.png' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   await page.click('button:has-text("Execute CLM")');
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step5.png' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   const runningText2 = await page.locator('h3', { hasText: 'Python Interactive Console' }).first();
   await runningText2.scrollIntoViewIfNeeded();
   await runningText2.focus();
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
   await page.waitForTimeout(1000);
   await page.locator("text=Running Python script...").waitFor({ timeout: 10000 });
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step6.png' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   await page.click('button:has-text("Clear")');
   await page.waitForTimeout(500);
   await page.screenshot({ path: 'step7.png' });
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
 
   await browser.close();
 })();
